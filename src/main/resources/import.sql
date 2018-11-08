db38ffbb5b251df7286495bdc98c922cbb94e1de
solomon <anhalt79@gmail.com>
2018-11-08 22:19:17 +0100

    new implements the login

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,4 +1,4 @@
 INSERT INTO role VALUES (1,'ADMIN');
-insert INTO user(user_id, email, name, last_name, active, password) VALUES (1, 'ff@mail.ru', 'eugene', 'utkin', 1, '$2a$10$DYQ62OzsxIptJGfha7n0zeF16Khx9Sd6Xke7PHK5e..Ik81c2icSy');
+insert INTO user(user_id, email, name, last_name, active, password) VALUES (1, 'ff@mail.de', 'eugene', 'utkin', 1, '$2a$10$DYQ62OzsxIptJGfha7n0zeF16Khx9Sd6Xke7PHK5e..Ik81c2icSy');
 INSERT INTO user_roles VALUES (1,1);
 insert into quote(id, text, author) VALUES (1, 'Hellow, World!', 1)
