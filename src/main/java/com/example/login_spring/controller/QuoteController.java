36638e2c0125173e4ba4abd3948c9e15e5372bdb
solomon <anhalt79@gmail.com>
2018-11-17 16:22:15 +0100

    -

--- src/main/java/com/example/login_spring/controller/QuoteController.java
+++ src/main/java/com/example/login_spring/controller/QuoteController.java
@@ -5,6 +5,7 @@ import com.example.login_spring.model.Quote;
 import com.example.login_spring.model.User;
 import com.example.login_spring.repository.QuoteRepository;
 
+import edu.umd.cs.findbugs.classfile.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -12,6 +13,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
 import java.util.List;
 
 
@@ -41,21 +43,23 @@ public class QuoteController {
         return "addQuote";
     }
 
-/*
 
-    @PostMapping("/update")
-    public String update(Quote quote, @AuthenticationPrincipal User author) {
-        quote.setAuthor(author);
+   /* @PutMapping("/update/{id}")
+    public String update(@PathVariable("id") Long quoteId, Quote quote) {
+        Quote quote1 `=  quoteRepository.findById( quoteId).orElseThrow(() -> new ResourceNotFoundException("Quote", "id", quoteId));;
+        quote1.setAuthor(quote.getAuthor());
+        quote1.setText(quote.getText())
         quoteRepository.save(quote);
+        Quote quoteupdate = quoteRepository.save(quote1);
         return "redirect:/ww";
     }
 
 */
 
 
-
     @PostMapping("/save")
     @RequestMapping(value = "/save", method = RequestMethod.POST)
+    @ExceptionHandler(IOException.class)
     public String save(Quote quote, @AuthenticationPrincipal User author) {
         quote.setAuthor(author);
         quoteRepository.save(quote);
